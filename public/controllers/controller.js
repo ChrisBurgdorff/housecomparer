9cc6dd6c6907c73c6bc6808c532be546d2d954ec
Chris Burgdorff <wesborland1234@aol.com>
2018-05-27 19:47:18 -0400

    Update controller.js

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,1 +1,9 @@
-var myApp = angular.module('myApp', []);myApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {	console.log("in controller.js");	$http.get('/houselist').success(function(response){		console.log("Got into request");		$scope.houseList = response;	});	}]);
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {
+	console.log("in controller.js");
+	$http.get('/house').success(function(response){
+		console.log("Got into request");
+		$scope.houseList = response;
+	});	
+}]);
