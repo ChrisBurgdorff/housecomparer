738f4ce50b9a305c2c6254bcff2092f50c0c8b8d
Chris Burgdorff <wesborland1234@aol.com>
2018-05-27 21:03:26 -0400

    Add files via upload

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,16 +1,1 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {
-	console.log("in controller.js");
-	$http({
-		method: 'GET',
-		url: '/house'
-	}).then(function(response) {
-		console.log("Got into request");
-		console.log(response);
-		//$scope.houseList = response;
-    $scope.houseList = [{address: 'sdfsd', belmar: 'dfv34'}];
-	}, function (error) {
-		console.log(error);
-	});
-}]);
+var myApp = angular.module('myApp', []);myApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {	console.log("in controller.js");	$http({		method: 'GET',		url: '/house'	}).then(function(response) {		console.log("Got into request");		console.log(response);		$scope.houseList = response;	}, function (error) {		console.log(error);	});}]);
