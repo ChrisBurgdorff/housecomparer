00151b4d0b7a93bb192b73591c3e049be7e44a7c
Chris Burgdorff <wesborland1234@aol.com>
2018-05-29 13:02:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser());
 
 //Connect to Mongo
-var db
+var db;
 
 MongoClient.connect(MongoURL, (err, client) => {
     if (err) return console.log(err)
-    db = client.db('housecomparerdb') // whatever your database name is
+    db = client.db('housecomparerdb'); // whatever your database name is
     app.listen(process.env.PORT || 3000, function(){
         console.log("Application is listening on port 3000, or whatever.");
     });
@@ -26,7 +26,7 @@ app.get('/house', function(req, res){
 	console.log("I RECIEVED THE GET REQUEST!!!!!!!!!");
     //Get houses from database
     db.collection('houses').find().toArray(function(err, results) {
-        console.log(results)
+        console.log(results);
         houseList = results;
     });
 	//var houseList = [house1, house2, house3];
