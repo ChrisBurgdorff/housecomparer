9840aece012ab442663045fb1e27cf3b586466f6
Chris Burgdorff <wesborland1234@aol.com>
2018-06-03 22:39:25 -0400

    Add files via upload

--- public/display.js
+++ public/display.js
@@ -1,3 +1,18 @@
-$(".link2").click(function(){
-  $(this).parent().parent().hide();
+$(document).ready(function(){
+
+	$(".link2").click(function(){
+		$(this).parent().parent().hide();
+	});
+	
+	$('.money').keyup(function(event) {
+		// skip for arrow keys
+		if(event.which >= 37 && event.which <= 40) return;
+		// format number
+		$(this).val(function(index, value) {
+			return value
+			.replace(/\D/g, "")
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		});
+	});
+	
 });
